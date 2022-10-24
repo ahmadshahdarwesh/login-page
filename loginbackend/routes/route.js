e7bf696f998e25c14dd892fd2baba32155898cc6
ahmadshahdarwesh <abadkaka@yahoo.com>
2022-10-24 11:45:57 +0200

    Schema created

--- loginbackend/routes/route.js
+++ loginbackend/routes/route.js
@@ -1,9 +1,18 @@
 const { response } = require("express");
 const express = require("express");
 const router = express.Router();
+const signupTeplateCopy = require("../models/Signupmodel");
 
 router.post("/signup", (req, res) => {
-  response.send("send");
+  const signedUpUser = new signupTeplateCopy({
+    fullName: req.body.fullName,
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  signedUpUser.save().then((data) => {
+    response.json(data);
+  });
 });
 
 module.exports = router;
